b8316d48787f75f569a4e0d106c3494b16ac5c12
Visinho <Elvisvisinho@rocketmail.com>
2025-08-09 23:14:31 +0100

    List and Delete food route

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,4 +1,5 @@
 import foodModel from "../models/foodModel.js";
+import path from "path";
 import fs from 'fs'
 
 // Add food item
@@ -37,4 +38,18 @@ const listFood = async(req, res) => {
     }
 }
 
-export {addFood, listFood}
+// Delete food item
+const deleteFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, ()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success: true, message: "Food has been deleted"})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error deleting food!"})
+    }
+}
+
+export {addFood, listFood, deleteFood}
